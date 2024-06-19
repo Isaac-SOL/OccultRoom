8b9d267481f49485e6520663ef82e437ebe5407d
Pierre Cuquel <p.cuquel@free.fr>
2024-06-19 20:27:48 +0200

    Added auto linker for PlaceableObjects

---
+++ scripts/General/object_placer.gd
@@ -0,0 +1,20 @@
+@tool 
+extends Node
+
+@export var object: PlaceableObject
+@export var point: ObjectPlacementPoint
+@export var click_to_place_object: bool : set = _click
+@export_multiline var info: String = "Assign the object and point you want to link then click the box."
+
+func _click(new_state: bool):
+	if object and point:
+		if object.holder:
+			object.holder.holding_object = null
+		if point.holding_object:
+			point.holding_object.holder = null
+		object.holder = point
+		point.holding_object = object
+		object.global_position = point.global_position
+		info = "Object linked to placement point!"
+	else:
+		info = "Assign the object and point you want to link then click the box."

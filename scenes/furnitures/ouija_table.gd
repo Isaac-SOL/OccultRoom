b8f3fff691329e25fb03f6b14897cc78ef668690
Pierre Cuquel <p.cuquel@free.fr>
2024-06-27 12:50:34 +0200

    Fixed merge and improved table outline

--- scenes/furnitures/ouija_table.gd
+++ scenes/furnitures/ouija_table.gd
@@ -3,11 +3,13 @@ extends Node3D
 @export var dissolve_material: ShaderMaterial
 @export var dissolve_material_candle: ShaderMaterial
 
-@onready var animation_tree = get_node("AnimationTree")
-@onready var playback = animation_tree.get("parameters/playback")
+@onready var animation_tree: AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animation_tree = $AnimationTree
+	playback = animation_tree.get("parameters/playback")
 	animation_tree.active = true
 
 func ouija_table_jump(): 
@@ -21,4 +23,5 @@ func appear():
 	tween.tween_method(func(val: float): dissolve_material.set_shader_parameter("offset", val), 0.5, -0.2, 1)
 	tween.parallel().tween_method(func(val: float): dissolve_material_candle.set_shader_parameter("offset", val), 0.5, -0.2, 1)
 	tween.tween_callback(func(): $ouija_board.set_surface_override_material(4, null))
+	tween.tween_callback(func(): $tapis_table.set_surface_override_material(1, null))
 	%AppearAudio.play()
